f08d025a14f960846d869368e75f2d2aed842e57
Seweryn Piórkowski <git@piorkowski.email>
2023-11-15 10:17:47 +0100

    replace file with template file in lambda policy

--- templates/source/create_app_sync_data_source/{{project_name}}/iam.tf
+++ templates/source/create_app_sync_data_source/{{project_name}}/iam.tf
@@ -7,7 +7,7 @@ resource "aws_iam_policy" "iam_policy_for_lambda" {
   name        = "aws-iam-policy-for-${aws_iam_role.lambda_role.name}"
   path        = "/"
   description = "AWS IAM Policy for managing ${aws_iam_role.lambda_role.name}"
-  policy      = file("${path.module}/iam_policy_for_lambda.json")
+  policy      = templatefile("${path.module}/iam_policy_for_lambda.json", {})
 }
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
